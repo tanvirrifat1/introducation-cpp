ef70df7b3f02186a72a509e6b424627f1a153e2c
tanvirrifat1 <rifatkhan5567790@gmail.com>
2025-11-18 17:23:43 +0600

    added

--- print.cpp
+++ print.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
+#include <iomanip>
+
 using namespace std;
 
 int main()
 {
 
-    int x;
+    double d = 50.545454;
 
-    cin >> x;
-
-    cout << x << endl;
+    cout << fixed << setprecision(6) << d << endl;
 
     return 0;
 }
