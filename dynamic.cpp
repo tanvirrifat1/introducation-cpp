bc340da9dd2acc8fc395a8e45d15332a5b38d8e8
tanvirrifat1 <rifatkhan5567790@gmail.com>
2025-11-19 16:05:37 +0600

    added dynamic array

---
+++ dynamic.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    // int a[5];
+    int *a = new int[5];
+
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        cout << a[i] << "\n";
+    }
+
+    return 0;
+}
